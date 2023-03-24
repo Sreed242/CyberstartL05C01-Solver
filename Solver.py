c6d8b737fb4ef7d61af0a7448b3cfbf86c4fa018
Sreed242 <128776899+Sreed242@users.noreply.github.com>
2023-03-24 00:20:06 -0500

    Update Solver.py
    Changed the text to make more sense

--- Solver.py
+++ Solver.py
@@ -1,6 +1,6 @@
-input_table2 = input("Enter the numbers in table 2   ")
+input_table2 = input("Enter the numbers to the right   ")
 table2 = input_table2.split()
-input_table1 = input("Enter the numbers in table 1   ")
+input_table1 = input("Enter the numbers to the left   ")
 table1 = input_table1.split()
 for num in range(len(table2)):
     table2[num] = int(table2[num])
